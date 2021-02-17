cec8dc021f8839430ea1e605a4d6565d6b413fe7
fancytseng <55032412+fancytseng@users.noreply.github.com>
2021-02-17 14:48:46 -0800

    Adding Code for view controller xib implementation

---
+++ TableViewXib/TableViewXib/TableViewXib/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  TableViewXib
+//
+//  Created by Fancy Tseng on 2/17/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblCity: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
