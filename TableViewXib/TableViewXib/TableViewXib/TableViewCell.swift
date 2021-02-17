590a1cb56133e84cc276b2d01bfc62dc2de0bc30
fancytseng <55032412+fancytseng@users.noreply.github.com>
2021-02-17 15:25:21 -0800

    Committing Cities Temperatures

--- TableViewXib/TableViewXib/TableViewXib/TableViewCell.swift
+++ TableViewXib/TableViewXib/TableViewXib/TableViewCell.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    
     @IBOutlet weak var lblCity: UILabel!
+    
+    @IBOutlet weak var lblValue: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- TableViewXib/TableViewXib/TableViewXib/TableViewController.swift
+++ TableViewXib/TableViewXib/TableViewXib/TableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class TableViewController: UITableViewController {
 
     let cities = ["Sesttle", "Portland", "SFO", "LA", "Diego", "NY", "Miami", "Colorado", "Austin"]
+    let values = ["46", "48", "61F", "57", "63", "30", "77", "16", "30"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +26,7 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = Bundle.main.loadNibNamed("TableViewCell", owner: self, options: nil)?.first as! TableViewCell
         cell.lblCity.text = cities[indexPath.row]
+        cell.lblValue.text = values[indexPath.row] + "F"
         return cell
     }
    
