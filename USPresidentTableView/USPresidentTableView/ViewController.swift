b6af113a44be8aa783ce268e2fd8971d8e21b083
fancytseng <55032412+fancytseng@users.noreply.github.com>
2021-03-04 03:12:30 -0800

    add USPresidentTableView assignment

---
+++ USPresidentTableView/USPresidentTableView/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  USPresidentTableView
+//
+//  Created by Fancy Tseng on 2/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tblView: UITableView!
+    
+    let names = ["George Washington", "John Adams", "Thomas Jerrferson", "Lyndon B. Johnson", "James Madison", "James Monroe", "John Quincy Adams", "Andrew Jackson"]
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            tblView.delegate = self
+            tblView.dataSource = self
+        }
+        
+
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return names.count;
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = names[indexPath.row]
+            return cell
+        }
+    }
+
